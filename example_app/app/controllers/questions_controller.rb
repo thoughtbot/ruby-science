29dc4acb194f68b53701a41aae65199960dc5261
Joe Ferris <jferris@thoughtbot.com>
2012-11-15 14:15:33 -0500

    Write about Long Method, Extract Method
    * Adds chapter for Long Method
    * Adds chapter for Extract Method
    * Demonstrates resolving a Long Method using Extract Method
    * Includes Replace Temp With Query

--- example_app/app/controllers/questions_controller.rb
+++ example_app/app/controllers/questions_controller.rb
@@ -9,15 +9,24 @@ class QuestionsController < ApplicationController
   def create
     @survey = Survey.find(params[:survey_id])
     @submittable_type = params[:submittable_type_id]
-    question_params = params.
-      require(:question).
-      permit(:submittable_type, :title, :options_attributes, :minimum, :maximum)
-    @question = @survey.questions.new(question_params)
-    @question.submittable_type = @submittable_type
+    build_question
     if @question.save
       redirect_to @survey
     else
       render :new
     end
+  end
+
+  private
+
+  def build_question
+    @question = @survey.questions.new(question_params)
+    @question.submittable_type = @submittable_type
+  end
+
+  def question_params
+    params.
+      require(:question).
+      permit(:submittable_type, :title, :options_attributes, :minimum, :maximum)
   end
 end
