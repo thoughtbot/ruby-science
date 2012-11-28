44f85d8a91135de58348b40f7e73b31b342944f4
Harlow Ward <harlow@thoughtbot.com>
2012-11-28 12:23:00 -0500

    User receives notification of completed survey
    * Notifies user when survey is complete
    * Introduces Long Parameter List small into Mailer

--- example_app/spec/spec_helper.rb
+++ example_app/spec/spec_helper.rb
@@ -13,6 +13,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.order = 'random'
-
+  config.mock_framework = :mocha
   config.include FactoryGirl::Syntax::Methods
 end
