b6c9a8d037f72f1b643f411f2fa217dd6dfebeeb
Joe Ferris <jferris@thoughtbot.com>
2012-11-28 14:29:05 -0500

    Display the email for each survey's author
    * Adds more behavior to User
    * Adds more behavior to surveys#create
    * Introduces Law of Demeter violation in surveys/show.html.erb

---
+++ example_app/spec/features/user_authors_survey_spec.rb
@@ -0,0 +1,14 @@
+feature 'user authors survey' do
+  include SurveySupport
+
+  scenario "attempt to edit another user's survey" do
+    sign_in
+    create_survey_with_title 'How are you?'
+    click_on 'How are you?'
+    page.should have_content("Author: #{current_user.email}")
+  end
+
+  def have_edit_links
+    have_css('a', text: /Add .*Question/)
+  end
+end
--- example_app/spec/support/mixins/session_support.rb
+++ example_app/spec/support/mixins/session_support.rb
@@ -1,10 +1,21 @@
 module SessionSupport
   def sign_in
-    user = create(:user, password: 'test')
+    @current_user = create(:user, password: 'test')
     visit '/sign_in'
-    fill_in 'Email', with: user.email
+    fill_in 'Email', with: @current_user.email
     fill_in 'Password', with: 'test'
     click_on 'Sign in'
+  end
+
+  def current_user
+    @current_user
+  end
+
+  def as_another_user
+    using_session 'other user' do
+      sign_in
+      yield
+    end
   end
 end
 
