d2435ee9eb29cd0b30c28d56ecdad7545ba2830e
Joe Ferris <jferris@thoughtbot.com>
2012-11-29 10:50:33 -0500

    Refactor out type check in QuestionsController
    * Relies on view code asking for correct options
    * Handle blank options logic in model
    * Removes Divergent Change smell from QuestsionController
    * Applies Replace Conditional with Polymorphism refactoring

--- example_app/app/models/multiple_choice_question.rb
+++ example_app/app/models/multiple_choice_question.rb
@@ -3,6 +3,14 @@ class MultipleChoiceQuestion < Question
 
   accepts_nested_attributes_for :options, reject_if: :all_blank
 
+  def options_for_form
+    if options.any?
+      options
+    else
+      [Option.new, Option.new, Option.new]
+    end
+  end
+
   def summary
     total = answers.count
     counts = answers.group(:text).order('COUNT(*) DESC').count
