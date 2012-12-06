c22463874a7dee1e2b4a997162b530e755e36f55
Joe Ferris <jferris@thoughtbot.com>
2012-12-06 13:37:48 -0500

    Remove duplication in Answer
    * Removes smell Duplicated Code
    * Applies refactoring Extract Method

--- example_app/app/models/answer.rb
+++ example_app/app/models/answer.rb
@@ -7,11 +7,16 @@ class Answer < ActiveRecord::Base
   validates :text, presence: true
 
   def self.for_user(user)
-    joins(:completion).where(completions: { user_id: user.id }).last ||
-      NullAnswer.new
+    joins(:completion).where(completions: { user_id: user.id }).last_or_null
   end
 
   def self.most_recent
-    order(:created_at).last || NullAnswer.new
+    order(:created_at).last_or_null
+  end
+
+  private
+
+  def self.last_or_null
+    last || NullAnswer.new
   end
 end
