1e3b7f754f89a0ae975f083e255fe66eee678739
Joe Ferris <jferris@thoughtbot.com>
2012-12-06 11:26:31 -0500

    Refactor 'user views survey results'
    * Extract class for creating survey content
    * Extract method for answering survey
    * Trim long fixture text
    * Overall goal: improve readability, reduce scenario complexity

--- example_app/spec/features/user_views_survey_results_spec.rb
+++ example_app/spec/features/user_views_survey_results_spec.rb
@@ -2,56 +2,36 @@ require 'spec_helper'
 
 feature 'user views survey results' do
   scenario 'view survey results' do
+    maker = SurveyMaker.new
+    maker.open_question 'Name?'
+    maker.multiple_choice_question 'Favorite color?', 'Red', 'Blue'
+    maker.scale_question 'Airspeed velocity?', 0..10
+    survey = maker.survey
+
     sign_in
-    survey = create(:survey)
-    create(
-      :open_question,
-      title: 'What is your name?',
-      survey: survey
-    )
-    create(
-      :multiple_choice_question,
-      title: 'What is your favorite color?',
-      options_texts: %w(Red Blue),
-      survey: survey
-    )
-    create(
-      :scale_question,
-      title: 'What is the airspeed velocity of a sparrow?',
-      minimum: 0,
-      maximum: 10,
-      survey: survey
-    )
+    answer_survey survey, 'Brian', 'Red', '10'
+    answer_survey survey, 'Billy', 'Red', '10'
+    answer_survey survey, 'Benny', 'Blue', '4'
 
-    view_survey survey
-    fill_in 'What is your name?', with: 'Brian'
-    choose 'Red'
-    choose '10'
-    submit_answers
-
-    view_survey survey
-    fill_in 'What is your name?', with: 'Billy'
-    choose 'Red'
-    choose '10'
-    submit_answers
-
-    view_survey survey
-    fill_in 'What is your name?', with: 'Benny'
-    choose 'Blue'
-    choose '4'
-    submit_answers
-
-    open_answer = summary_for_question('What is your name?')
+    open_answer = summary_for_question('Name?')
     open_answer.should have_content('Brian')
     open_answer.should have_content('Billy')
     open_answer.should have_content('Benny')
 
-    multiple_choice_answer = summary_for_question('What is your favorite color?')
+    multiple_choice_answer = summary_for_question('Favorite color?')
     multiple_choice_answer.should have_content('67% Red')
     multiple_choice_answer.should have_content('33% Blue')
 
-    slider_answer = summary_for_question('What is the airspeed velocity of a sparrow?')
+    slider_answer = summary_for_question('Airspeed velocity?')
     slider_answer.should have_content('Average: 8')
+  end
+
+  def answer_survey(survey, name, color, airspeed_velocity)
+    view_survey survey
+    fill_in 'Name?', with: name
+    choose color
+    choose airspeed_velocity
+    submit_answers
   end
 
   def summary_for_question(question)
