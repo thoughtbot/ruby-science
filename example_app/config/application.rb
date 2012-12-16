a6dd34e30835b8c16866276483afc2531c3d782c
Harlow Ward <harlow@thoughtbot.com>
2012-12-16 12:45:10 -0500

    Get demo app working in web browser

--- example_app/config/application.rb
+++ example_app/config/application.rb
@@ -5,7 +5,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
@@ -59,5 +59,7 @@ module ExampleApp
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = false
 
+    # Enable the asset pipeline
+    config.assets.enabled = true
   end
 end
