21d48fb60aa7b3dcc503a8f2a1ae96cc1c406b3e
Joe Ferris <jferris@thoughtbot.com>
2013-01-31 14:40:45 -0500

    Replace Subclasses with Strategies: Step Three
    * Move remaining common API into strategies

--- example_app/app/models/open_submittable.rb
+++ example_app/app/models/open_submittable.rb
@@ -1,5 +1,20 @@
 class OpenSubmittable
+  def initialize(question)
+    @question = question
+  end
+
+  def breakdown
+    text_from_ordered_answers = answers.order(:created_at).pluck(:text)
+    text_from_ordered_answers.join(', ')
+  end
+
   def score(text)
     0
+  end
+
+  private
+
+  def answers
+    @question.answers
   end
 end
--- example_app/spec/models/scale_submittable_spec.rb
+++ example_app/spec/models/scale_submittable_spec.rb
@@ -1,6 +1,21 @@
+describe ScaleSubmittable, '#breakdown' do
+  it 'returns the average' do
+    survey = create(:survey)
+    question = create(:scale_question, minimum: 0, maximum: 10, survey: survey)
+    submittable = ScaleSubmittable.new(question)
+    taker = AnswerCreator.new(survey)
+    taker.answer question, 6
+    taker.answer question, 6
+    taker.answer question, 8
+
+    submittable.breakdown.should eq 'Average: 6.67'
+  end
+end
+
 describe ScaleSubmittable, '#score' do
   it 'returns the integer value of the text' do
-    submittable = ScaleSubmittable.new
+    question = build_stubbed(:scale_question)
+    submittable = ScaleSubmittable.new(question)
 
     result = submittable.score('5')
 
