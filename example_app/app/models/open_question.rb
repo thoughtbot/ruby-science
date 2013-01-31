4757eb0afdb48bca50c54d7ef343357bb33cf50b
Joe Ferris <jferris@thoughtbot.com>
2013-01-31 14:28:25 -0500

    Replace Subclasses with Strategies: Step Two
    * Pull up delegate method into base class

--- example_app/app/models/open_question.rb
+++ example_app/app/models/open_question.rb
@@ -1,8 +1,4 @@
 class OpenQuestion < Question
-  def score(text)
-    submittable.score(text)
-  end
-
   def breakdown
     text_from_ordered_answers = answers.order(:created_at).pluck(:text)
     text_from_ordered_answers.join(', ')
