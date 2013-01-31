4aa592afa77d6e5cf3bec05244dc2c05f9a841fb
Joe Ferris <jferris@thoughtbot.com>
2013-01-31 15:20:56 -0500

    Replace Subclasses with Strategies: Step Five
    * Pass strategy instead of subclass when only the strategy is needed
    * Don't delegate strategy-specific behavior from subclasses

--- example_app/app/models/multiple_choice_question.rb
+++ example_app/app/models/multiple_choice_question.rb
@@ -3,10 +3,6 @@ class MultipleChoiceQuestion < Question
 
   accepts_nested_attributes_for :options, reject_if: :all_blank
 
-  def options_for_form
-    submittable.options_for_form
-  end
-
   def submittable
     MultipleChoiceSubmittable.new(self)
   end
--- example_app/app/models/scale_question.rb
+++ example_app/app/models/scale_question.rb
@@ -2,10 +2,6 @@ class ScaleQuestion < Question
   validates :maximum, presence: true
   validates :minimum, presence: true
 
-  def steps
-    submittable.steps
-  end
-
   def submittable
     ScaleSubmittable.new(self)
   end
