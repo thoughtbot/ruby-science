dd6b4862619304f22184fdd488439a1873266979
Joe Ferris <jferris@thoughtbot.com>
2013-02-07 17:18:40 -0500

    Replace Subclasses with Strategies: Step Fifteen
    * Simplify type switching

--- example_app/app/controllers/types_controller.rb
+++ example_app/app/controllers/types_controller.rb
@@ -1,36 +1,24 @@
 class TypesController < ApplicationController
   def new
     @question = Question.find(params[:question_id])
-    assign_type
-    @new_question = Question.new
-    @new_question.build_submittable(type, {})
+    @question.build_submittable(type, {})
   end
 
   def create
     @question = Question.find(params[:question_id])
-    @new_question = @question.switch_to(
+    @question.switch_to(
       type,
-      question_params,
       submittable_attributes
     )
 
-    if @new_question.errors.empty?
+    if @question.errors.empty?
       redirect_to @question.survey
     else
-      assign_type
       render 'new'
     end
   end
 
   private
-
-  def assign_type
-    @new_type = type.underscore
-  end
-
-  def question_params
-    params[:question].except(:submittable_attributes)
-  end
 
   def submittable_attributes
     if params[:question][:submittable_attributes]
