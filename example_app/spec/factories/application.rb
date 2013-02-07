716f356b7f1f4adeaa1693fa82ee4b9554b09ca3
Joe Ferris <jferris@thoughtbot.com>
2013-02-07 14:39:24 -0500

    Replace Subclasses with Strategies: Step Eight
    * Introduce polymorphic association for strategy
    * Build the strategy when building the root type

--- example_app/spec/factories/application.rb
+++ example_app/spec/factories/application.rb
@@ -10,6 +10,12 @@ FactoryGirl.define do
     user
   end
 
+  factory :multiple_choice_submittable do
+  end
+
+  factory :open_submittable do
+  end
+
   factory :option do
     text 'Hello'
   end
@@ -28,15 +34,23 @@ FactoryGirl.define do
           FactoryGirl.build(:option, text: text, question_id: attributes.id)
         end
       end
+
+      submittable factory: :multiple_choice_submittable
     end
 
     factory :open_question, class: 'OpenQuestion' do
+      submittable factory: :open_submittable
     end
 
     factory :scale_question, class: 'ScaleQuestion' do
       minimum 1
       maximum 2
+
+      submittable factory: :scale_submittable
     end
+  end
+
+  factory :scale_submittable do
   end
 
   factory :survey do
