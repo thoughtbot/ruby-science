21313ba301a3b1d3e9c691bb2ce1d5b95736b2d1
Joe Ferris <jferris@thoughtbot.com>
2013-02-21 15:47:37 -0500

    Extract Decorator Step Two
    * Move conditional method into decorator

--- example_app/app/models/unanswered_question_hider.rb
+++ example_app/app/models/unanswered_question_hider.rb
@@ -4,4 +4,8 @@ class UnansweredQuestionHider
   def hide_answer_to_question(question)
     Summary.new(question.title, NO_ANSWER)
   end
+
+  def hide_unanswered_question?(question, user)
+    user && !question.answered_by?(user)
+  end
 end
--- example_app/spec/models/unanswered_question_hider_spec.rb
+++ example_app/spec/models/unanswered_question_hider_spec.rb
@@ -11,3 +11,34 @@ describe UnansweredQuestionHider, '#hide_answer_to_question' do
     result.value.should eq UnansweredQuestionHider::NO_ANSWER
   end
 end
+
+describe UnansweredQuestionHider, '#hide_unanswered_question?' do
+  it 'returns true given a user without an answer' do
+    user = build_stubbed(:user)
+    question = stub_answered_question(user, false)
+    hider = UnansweredQuestionHider.new
+
+    hider.hide_unanswered_question?(question, user).should be_truthy
+  end
+
+  it 'returns false given a user with an answer' do
+    user = build_stubbed(:user)
+    question = stub_answered_question(user, true)
+    hider = UnansweredQuestionHider.new
+
+    hider.hide_unanswered_question?(question, user).should be_falsey
+  end
+
+  it 'returns false without a user' do
+    question = build_stubbed(:question)
+    hider = UnansweredQuestionHider.new
+
+    hider.hide_unanswered_question?(question, nil).should be_falsey
+  end
+
+  def stub_answered_question(user, answered)
+    build_stubbed(:question).tap do |question|
+      question.stubs(:answered_by?).with(user).returns(answered)
+    end
+  end
+end
