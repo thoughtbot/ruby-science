4d740d60b9446834f934176ce4c7c92735bd6484
Joe Ferris <jferris@thoughtbot.com>
2013-02-21 16:00:57 -0500

    Extract Decorator Step Three
    * Move decorator body into decorator

--- example_app/app/models/unanswered_question_hider.rb
+++ example_app/app/models/unanswered_question_hider.rb
@@ -1,6 +1,16 @@
 class UnansweredQuestionHider
   NO_ANSWER = "You haven't answered this question".freeze
 
+  def summary_or_hidden_answer(summarizer, question, user)
+    if hide_unanswered_question?(question, user)
+      hide_answer_to_question(question)
+    else
+      question.summary_using(summarizer)
+    end
+  end
+
+  private
+
   def hide_answer_to_question(question)
     Summary.new(question.title, NO_ANSWER)
   end
