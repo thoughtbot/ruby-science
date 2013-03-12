2051bab6e8c924fe71b78b29710573657e7aa839
Joe Ferris <jferris@thoughtbot.com>
2013-03-12 17:47:56 -0400

    Inject Dependencies: Step Two
    * Remove unnecessary sub-dependencies

--- example_app/spec/models/question_spec.rb
+++ example_app/spec/models/question_spec.rb
@@ -77,9 +77,8 @@ describe Question, '#summary_using' do
   it 'builds a summary with the result from the summarizer' do
     question = build_stubbed(:question)
     summarizer = stub('summarizer', summarize: 'result')
-    options = 'options'
 
-    summary = question.summary_using(summarizer, options)
+    summary = question.summary_using(summarizer)
 
     summary.title.should eq question.title
     summary.value.should eq 'result'
