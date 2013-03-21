6b5273dde4ac55fe61fc317635eace84c85b2f61
Joe Ferris <jferris@thoughtbot.com>
2013-03-21 11:52:22 -0400

    Remove private messages
    * Staging for solution Inline Class

--- example_app/app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ActiveRecord::Base
-  belongs_to :recipient, class_name: 'User'
-  belongs_to :sender, class_name: 'User'
-
-  validates :body, presence: true
-  validates :recipient_id, presence: true
-  validates :sender_id, presence: true
-end
