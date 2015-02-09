de87c011d6e9934d7896fefe0045a3258e13a053
Joe Ferris <jferris@thoughtbot.com>
2015-02-09 14:43:29 -0500

    Update dependencies
    * Update Rails
    * Updates RSpec
    * Replaces Bourne with rspec-mocks
    * Updates configuration
    * Adds binstubs

--- example_app/spec/spec_helper.rb
+++ example_app/spec/spec_helper.rb
@@ -1,18 +1,26 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
+ActiveRecord::Migration.maintain_test_schema!
+
 RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.order = :random
   config.use_transactional_fixtures = true
-  config.infer_base_class_for_anonymous_controllers = false
-  config.order = 'random'
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 
   config.include FactoryGirl::Syntax::Methods
 end
